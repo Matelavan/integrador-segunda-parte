005e4349fc9944c654e267233ee4db9fc53198a4
matiasroyann <matiasroyan11@gmail.com>
2025-05-31 14:37:31 -0300

    Home(hay que ver si funciona) y funcionalidad de like hechos

--- src/screens/home.js
+++ src/screens/home.js
@@ -3,6 +3,7 @@ import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native
 import { onSnapshot, collection, query, orderBy, updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase/config';
 import { getAuth } from 'firebase/auth';
+import LikeButton from '../components/LikeButton'; 
 
 class Home extends Component {
   constructor(props) {
@@ -28,19 +29,7 @@ class Home extends Component {
     if (this.unsubscribe) this.unsubscribe();
   }
 
-  toggleLike = (post) => {
-    const postRef = doc(db, 'posts', post.id);
-    const likes = post.likes || [];
-    const userEmail = this.state.user.email;
-
-    const newLikes = likes.includes(userEmail)
-      ? likes.filter(email => email !== userEmail)
-      : [...likes, userEmail];
-
-    updateDoc(postRef, { likes: newLikes })
-      .then(() => console.log('Like actualizado'))
-      .catch(error => console.error('Error al actualizar like:', error));
-  };
+ 
 
   renderItem = ({ item }) => {
     const userLiked = item.likes?.includes(this.state.user.email);
@@ -49,9 +38,7 @@ class Home extends Component {
         <Text style={styles.author}>{item.userEmail}</Text>
         <Text style={styles.content}>{item.text}</Text>
         <Text style={styles.likes}>Likes: {item.likes?.length || 0}</Text>
-        <TouchableOpacity onPress={() => this.toggleLike(item)}>
-          <Text style={styles.likeButton}>{userLiked ? 'Quitar Me gusta' : 'Me gusta'}</Text>
-        </TouchableOpacity>
+        <LikeButton postId={item.id} likes={item.likes || []} userEmail={this.state.user.email} />
       </View>
     );
   };
