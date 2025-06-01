018cc6f71f884e49d6ec6b3f0997419e9c3f6345
matiasroyann <matiasroyan11@gmail.com>
2025-06-01 18:05:43 -0300

    saque algunos comentarios

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 
 import { Text, View, TouchableOpacity } from 'react-native';
-//import Componente from './src/components/Componente'
 import MainNav from "./src/navegacion/mainNav"
 
 export default function App() {
